0ffa65d08337ab50e4cef4789fad2e903a13dd84
Owen Oliveira <ooliveira9359@gmail.com>
2024-10-11 11:08:29 -0400

    Added basic hub code to get framework set for the different home security hubs

---
+++ ProjectV/ProjectV/Hubs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectV
+{
+    public class LockHub : HomeSecurityHub
+    {
+        // not sure if i should ahve them set to false
+        public LockHub(bool isActive = false) : base("Lock") { }
+    }
+
+    public class SensorHub : HomeSecurityHub
+    {
+        public SensorHub() : base("Sensor") { }
+    }
+
+    public class CameraHub : HomeSecurityHub
+    {
+        public CameraHub() : base("Camera") { }
+    }
+
+    public class AlarmHub : HomeSecurityHub
+    {
+        public AlarmHub() : base("Alarm") { }
+    }
+
+    public class TrackerHub : HomeSecurityHub
+    {
+        public TrackerHub() : base("Tracker") { }
+    }
+}
