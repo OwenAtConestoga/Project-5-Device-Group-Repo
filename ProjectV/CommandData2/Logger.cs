bee2c1f6d8e227186585b8722e2242acad867638
Ethan Major <ethanmajor9@gmail.com>
2024-10-14 14:05:27 -0400

    Added basic device code to framework future smart devices off of

---
+++ ProjectV/CommandData2/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Devices
+{
+    internal static class Logger
+    {
+        const string logFilePath = "log.txt";
+
+        public enum LogType
+        {
+            Info,
+            Alert,
+            Error
+        }
+
+        public static void Log(string message, LogType type)
+        {
+            var timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            var logEntry = $"{timestamp}, {type}, {message}";
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine(logEntry);
+            }
+        }
+    }
+}
---
+++ ProjectV/CommandData2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Devices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = new Device();
+            device.PrintCurrentState();
+            Task.Run(() => device.StartDeviceAsync("127.0.0.1", 8080));
+
+            Thread.Sleep(100000);
+            device.StopDevice();
+        }
+    }
+}
