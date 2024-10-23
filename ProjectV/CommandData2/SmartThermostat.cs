2ee18252b7690ffd3d5588f9cc6017d74fb9179f
Patrick Cho <patrickjohncho@gmail.com>
2024-10-23 10:08:12 -0400

    Actually added interface for the thermostat

---
+++ ProjectV/CommandData2/SmartThermostat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommandData2
+{
+    public partial class SmartThermostat : Form
+    {
+        public SmartThermostat()
+        {
+            InitializeComponent();
+        }
+
+        private void powerButton_Click(object sender, EventArgs e)
+        {
+            //Event Handler for powerbutton click
+        }
+
+        private void tempUpButton_Click(object sender, EventArgs e)
+        {
+            //Event handler for temp up button click
+        }
+
+        private void tempDownButton_Click(object sender, EventArgs e)
+        {
+            //Event handler for temp down button click
+        }
+    }
+}
